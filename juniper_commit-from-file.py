2fed7c9216ee33b4964da6225e7d8dfbd84d85cd
kalyanjis700 <60326885+kalyanjis700@users.noreply.github.com>
2020-02-10 00:35:04 +0530

    updated for 2.7 version

--- juniper_commit-from-file.py
+++ juniper_commit-from-file.py
@@ -1,36 +1,33 @@
 from netmiko import ConnectHandler
 import getpass
+from datetime import datetime
 import time
-
+start_time = datetime.now()
 print('Banner')
-config = input('Enter config file name : ')
-device_list = input('Enter Device name seperated by "," : ')
+config = raw_input(str('Enter config file name :  '))
+device_list = raw_input(str('Enter Device name seperated by "," : '))
 device_list = list(device_list.split(','))
-username =  input('Login as : ')
+username =  raw_input('Login as : ')
 password =  getpass.getpass('password : ')
 print(device_list)
 time.sleep(1)
 for devices in device_list:
-     
-    connect = ConnectHandler(device_type='juniper',ip=devices,username=username,password=password,port=22)
+    connect = ConnectHandler(device_type='juniper',ip=devices,username=username,password=password,port=22,global_delay_factor=2)
     connect.find_prompt()
     time.sleep(1)
     print('Connect Successfully')
     connect.config_mode()
-    with open (config, 'r') as f:
-        command=f.read().splitlines()
-    for line in command:
-        print(line)
-        connect.send_config_set(line)
-        connect.exit_config_mode()
-    print(connect.send_command('show | compare'))
-    x = input('Proceed to commit(Y or N) : ')
+    connect.send_config_from_file(config)
+    print(connect.send_config_set('show | compare'))
+    print(connect.send_config_set('commit check'))
+    x = raw_input(str('Proceed to commit(Y or N) : '))
     if x == 'Y':
         print ('Commiting')
-        connect.commit()
+        connect.send_config_set('commit')
         connect.exit_config_mode()
-        time.sleep(1)
         connect.disconnect()
     else:
         connect.disconnect()
+end_time = datetime.now()
+print("Total time: {}".format(end_time - start_time))
 print ('Completed Successfully!!')
